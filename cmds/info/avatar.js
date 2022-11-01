a9a705a0318cc6775de15477b089f8b8033656a8
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2022-11-01 19:42:00 -0400

    i give up

--- cmds/info/avatar.js
+++ cmds/info/avatar.js
@@ -2,10 +2,10 @@ const { Embed } = require("guilded.ts");
 
 module.exports = {
   async execute(msg, args, client, config) {
-    console.log(`---------------------\n${msg.author}\n---------------------`);
-    if (msg.mentions?.users?.length > 0) console.log(msg.mentions.users[0]);
-    // use '?' syntax to get msg.mentions.users[0] if it exists, then use client.users.cache.get to get the user, otherwise use msg.author
-    let user = msg.mentions?.users[0] ? client.users.cache.get(msg.mentions.users[0].id) : msg.author;
+    // check if a user was mentioned, make it a boolean
+    const mentioned = msg.mentions?.users?.length > 0;
+    // if a user was mentioned, get the user object with client.users.fetch, otherwise get the author
+    const user = mentioned ? client.users.fetch(msg.serverId, msg.mentions.users[0].id) : msg.author;
 
     let av = user.avatar? user.avatar : "https://cdn.discordapp.com/embed/avatars/0.png";
 
